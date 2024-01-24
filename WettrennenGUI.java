fea9a1f15142b3ec8f9278f5108e362cba731a6c
InsideOut92 <146266019+InsideOut92@users.noreply.github.com>
2024-01-24 12:13:49 +0100

    WettrennenGUI.java

--- WettrennenGUI.java
+++ WettrennenGUI.java
@@ -2,10 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class WettrennenGUI{
+public class WettrennenGUI {
     JFrame frame = new JFrame("Wettrennen");
     JLabel anzahlTeilnehmer = new JLabel("Anzahl der Teilnehmer");
     JTextField teilnehmerTF = new JTextField();
@@ -21,14 +22,17 @@ public class WettrennenGUI{
     PlatzierungsPanel secondPlace = new PlatzierungsPanel();
     PlatzierungsPanel thirdPlace = new PlatzierungsPanel();
 
-    CheckBoxPanel suv_panel = new CheckBoxPanel("SUV");
-    CheckBoxPanel traktor_panel = new CheckBoxPanel("Traktor");
-    CheckBoxPanel motorrad_panel = new CheckBoxPanel("Motorrad");
-    CheckBoxPanel rennauto_panel = new CheckBoxPanel("Rennauto");
+    CheckBoxPanel suv_panel;
+    CheckBoxPanel traktor_panel;
+    CheckBoxPanel motorrad_panel;
+    CheckBoxPanel rennauto_panel;
+    CheckBoxPanel lkw_panel;
+    CheckBoxPanel fahrrad_panel;
 
-    public WettrennenGUI(){}
+    public WettrennenGUI() {
+    }
 
-    private void createAndShowGUI(){
+    private void createAndShowGUI() {
         JPanel panel = new JPanel();
 
         GridBagLayout gbl = new GridBagLayout();
@@ -36,11 +40,31 @@ public class WettrennenGUI{
         panel.setLayout(gbl);
         gbl.setConstraints(panel, gbc);
 
+        // Bilder für vorhandene Fahrzeugklassen laden
+        ImageIcon suvIcon = createImageIcon("/pfad/zum/suv_bild.png");
+        ImageIcon traktorIcon = createImageIcon("/pfad/zum/traktor_bild.png");
+        ImageIcon motorradIcon = createImageIcon("/pfad/zum/motorrad_bild.png");
+        ImageIcon rennautoIcon = createImageIcon("/pfad/zum/rennauto_bild.png");
+        ImageIcon lkwIcon = createImageIcon("/pfad/zum/lkw_bild.png");
+        ImageIcon fahrradIcon = createImageIcon("/pfad/zum/fahrrad_bild.png");
+
+        // Checkboxen für Fahrzeugklassen erstellen und Bilder zuweisen
+        suv_panel = new CheckBoxPanel("SUV", suvIcon);
+        traktor_panel = new CheckBoxPanel("Traktor", traktorIcon);
+        motorrad_panel = new CheckBoxPanel("Motorrad", motorradIcon);
+        rennauto_panel = new CheckBoxPanel("Rennauto", rennautoIcon);
+        // neu
+        lkw_panel = new CheckBoxPanel("LKW", lkwIcon);
+        fahrrad_panel = new CheckBoxPanel("Fahrrad", fahrradIcon);
+
         vehiclePanel.setLayout(new BoxLayout(vehiclePanel, BoxLayout.X_AXIS));
         vehiclePanel.add(suv_panel);
         vehiclePanel.add(traktor_panel);
         vehiclePanel.add(motorrad_panel);
         vehiclePanel.add(rennauto_panel);
+        // neu
+        vehiclePanel.add(lkw_panel);
+        vehiclePanel.add(fahrrad_panel);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -89,53 +113,61 @@ public class WettrennenGUI{
         frame.setVisible(true);
     }
 
-    public void main(){
-        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+    public void main() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 createAndShowGUI();
                 setButtonListener();
             }
         });
     }
 
-    private void setButtonListener(){
-        ActionListener startListener = new ActionListener(){
+    private void setButtonListener() {
+        ActionListener startListener = new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 int rundenZahl = 0;
-                try{
+                try {
                     rundenZahl = Integer.parseInt(rundenTF.getText());
-                } catch(Exception exception){
+                } catch (Exception exception) {
                     rundenZahl = 6;
                 }
 
                 int teilnehmerZahl = 0;
-                try{
+                try {
                     teilnehmerZahl = Integer.parseInt(teilnehmerTF.getText());
-                } catch(Exception exception){
+                } catch (Exception exception) {
                     teilnehmerZahl = 10;
                 }
 
                 ArrayList<String> vehicleList = new ArrayList<>();
-                if(traktor_panel.checkBox.isSelected()){
+                if (traktor_panel.checkBox.isSelected()) {
                     vehicleList.add("Traktor");
                 }
 
-                if(suv_panel.checkBox.isSelected()) {
+                if (suv_panel.checkBox.isSelected()) {
                     vehicleList.add("SUV");
                 }
 
-                if(rennauto_panel.checkBox.isSelected()) {
+                if (rennauto_panel.checkBox.isSelected()) {
                     vehicleList.add("Rennauto");
                 }
 
                 if (motorrad_panel.checkBox.isSelected()) {
                     vehicleList.add("Motorrad");
                 }
-
+                // neu
+                if (lkw_panel.checkBox.isSelected()) {
+                    vehicleList.add("LKW");
+                }
+                // neu
+                if (fahrrad_panel.checkBox.isSelected()) {
+                    vehicleList.add("Fahrrad");
+                }
+                // aktualisiert
                 if (vehicleList.size() == 0) {
-                    Collections.addAll(vehicleList, "Rennauto", "SUV", "Traktor", "Motorrad");
+                    Collections.addAll(vehicleList, "Rennauto", "SUV", "Traktor", "Motorrad", "LKW", "Fahrrad");
                 }
 
                 String[] vehicleArray = vehicleList.toArray(new String[vehicleList.size()]);
@@ -163,21 +195,20 @@ public class WettrennenGUI{
 
                 platzierungsLabel.setText("<html>Platzierung: <br/> " +
                         "1.Platz: " + top3Liste.get(0).getKennzeichen() + " : " + top3Liste.get(0).getRennstrecke() + " <br/> " +
-                        "2.Platz: " + top3Liste.get(1).getKennzeichen() + " : " + top3Liste.get(1).getRennstrecke()+ " <br/> " +
-                        "3.Platz: " + top3Liste.get(2).getKennzeichen() + " : " + top3Liste.get(2).getRennstrecke()+ " </html>");
+                        "2.Platz: " + top3Liste.get(1).getKennzeichen() + " : " + top3Liste.get(1).getRennstrecke() + " <br/> " +
+                        "3.Platz: " + top3Liste.get(2).getKennzeichen() + " : " + top3Liste.get(2).getRennstrecke() + " </html>");
             }
         };
         startButton.addActionListener(startListener);
     }
+    // neue Methode
+    private ImageIcon createImageIcon(String path) {
+        URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Bild konnte nicht geladen werden: " + path);
+            return null;
+        }
+    }
 }
-
-// ToDone: Fahrzeugklassen über Bilder+Checkbox auswählen für den RandomGenerator
-    // ToDone: Bild raussuchen für die Checkboxen
-    // ToDone: Bilder in Projekt einfügen
-    // ToDone: Bilder in die Checkboxen einbinden
-    // ToDone: Bilder in der Größe anpassen
-    // ToDone: RandomGenerator modifizieren, dass er nur die ausgewählten Fahrzeugklassen nimmt
-    // ToDone: Anpassungen in der Rennerstellung
-    // ToDone: Liste aus ausgewählten Bildern erstellen
-
-
